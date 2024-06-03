ddcdd85efe0a6d8736f23b5048df94fa80d40ffa
Natalia Justicia <nataliajusticia@gmail.com>
2024-06-03 14:52:01 +0200

    refactor: Remove unused code and update NodeHiveConfig in jsonapi-config.ts

--- src/nodehive/jsonapi-config.ts
+++ src/nodehive/jsonapi-config.ts
@@ -1,22 +1,13 @@
 export const NodeHiveConfig = {
   entities: {
-    'node-page': {
-      addFilter: [['status', '1']],
-      addFields: [
-        'title',
-        'field_media',
-        'field_paragraphs',
-        'field_paragraphs.field_image.field_media_image',
-      ],
-      addInclude: [
-        'field_media.field_media_image',
-        'field_paragraphs',
-        'field_paragraphs.field_image.field_media_image',
-      ],
-    },
-    'node-article': {
-      addFields: ['title', 'field_tags'],
-      addInclude: ['field_tags'],
-    },
+    // 'node-page': {
+    //   addFilter: [['status', '1']],
+    //   addFields: ['title'],
+    //   addInclude: [
+    //     'field_media.field_media_image',
+    //     'field_paragraphs',
+    //     'field_paragraphs.field_image.field_media_image',
+    //   ],
+    // },
   },
 };
