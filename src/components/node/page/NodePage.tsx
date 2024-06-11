2372a16ad7c183903b1f8427a41ad6de8214c682
Natalia Justicia <nataliajusticia@gmail.com>
2024-06-11 15:44:57 +0200

    feat: Update NodePage and Paragraph components

--- src/components/node/page/NodePage.tsx
+++ src/components/node/page/NodePage.tsx
@@ -29,8 +29,10 @@ export default function NodePage({ node }: NodePageProps) {
         </div>
       )}
 
-      <details className="container mx-auto mb-10 mt-10 rounded-md bg-black p-8 px-4 text-xs text-slate-50">
-        <summary className="cursor-pointer font-bold">API JSON Output</summary>
+      <details className="container mx-auto mb-10 mt-10 rounded-md bg-black p-4 text-xs text-slate-50">
+        <summary className="cursor-pointer font-bold">
+          Node API JSON Output
+        </summary>
         <pre className="mt-8">{JSON.stringify(node, null, 2)}</pre>
       </details>
     </article>
--- src/components/paragraph/Paragraph.tsx
+++ src/components/paragraph/Paragraph.tsx
@@ -19,5 +19,16 @@ export default function Paragraph({ paragraph }: ParagraphProps) {
     );
   }
 
-  return null;
+  return (
+    <section data-paragraph-type="Unknown">
+      <h2>Unknown paragraph type: {paragraphType}</h2>
+
+      <details className="container mx-auto mt-2 rounded-md bg-black p-4 text-xs text-slate-50">
+        <summary className="cursor-pointer font-bold">
+          Paragraph API:JSON Output
+        </summary>
+        <pre className="mt-8">{JSON.stringify(paragraph, null, 2)}</pre>
+      </details>
+    </section>
+  );
 }
