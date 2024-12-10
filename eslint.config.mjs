5995c8e44b6c258ac2f21e05c76000394cfa3427
Yannick Bättig <yannick.baettig@netnode.ch>
2024-12-10 09:01:24 +0100

    migrate eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+const eslintConfig = [...compat.extends('next/core-web-vitals', 'prettier')];
+export default eslintConfig;
