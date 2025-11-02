dbded8be6f29cddfba8cb0993dba387f318a2e42
Yannick Bättig <yannick.baettig@netnode.ch>
2025-11-02 22:02:58 +0100

    Improve layout

--- src/app/[lang]/forbidden.tsx
+++ src/app/[lang]/forbidden.tsx
@@ -1,15 +1,19 @@
 import Link from 'next/link';
 
+import Container from '@/components/theme/atoms-layout/container/container';
+
 export default function Forbidden() {
   return (
-    <section className="space-y-8">
-      <h1>403 - Forbidden</h1>
+    <Container className="py-16">
+      <section className="space-y-8">
+        <h1>403 - Forbidden</h1>
 
-      <p>You do not have permission to access this resource.</p>
+        <p>You do not have permission to access this resource.</p>
 
-      <Link href="/" className="btn-primary">
-        Go Back Home
-      </Link>
-    </section>
+        <Link href="/" className="btn-primary">
+          Go Back Home
+        </Link>
+      </section>
+    </Container>
   );
 }
--- src/app/[lang]/not-found.tsx
+++ src/app/[lang]/not-found.tsx
@@ -1,20 +1,22 @@
-import Link from 'next/link';
-
+import CallToAction from '@/components/theme/atoms-content/call-to-action/call-to-action';
 import { H1 } from '@/components/theme/atoms-content/heading/heading';
+import Container from '@/components/theme/atoms-layout/container/container';
 
 export default function NotFound() {
   return (
-    <section className="space-y-8">
-      <H1>404 - Page Not Found</H1>
+    <Container className="py-16">
+      <section className="space-y-8">
+        <H1>404 - Page Not Found</H1>
 
-      <p>
-        The page you are looking for might have been removed, had its name
-        changed, or is temporarily unavailable.
-      </p>
+        <p>
+          The page you are looking for might have been removed, had its name
+          changed, or is temporarily unavailable.
+        </p>
 
-      <Link href="/" className="btn-primary">
-        Go Back Home
-      </Link>
-    </section>
+        <CallToAction href="/" variant={'button'}>
+          Go Back Home
+        </CallToAction>
+      </section>
+    </Container>
   );
 }
